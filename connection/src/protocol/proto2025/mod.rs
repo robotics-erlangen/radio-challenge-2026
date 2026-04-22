bd6d76268fc1648c269e74059daca4034d317a64
Maximilian Hausen <maximilian.hausen@robotics-erlangen.de>
2026-04-22 20:27:01 +0200

    crates: connection: Tests for the 2025 protocol

--- connection/src/protocol/proto2025/mod.rs
+++ connection/src/protocol/proto2025/mod.rs
@@ -160,12 +160,12 @@ impl<
     fn next_packet(&mut self, regular_data: RC) -> Vec<u8> {
         let mut packet = vec![0; PACKET_SIZE];
 
-        self.counter = (self.counter + 1) % (2u8.pow(6));
         let mut header = PacketHeader {
             counter: self.counter,
             acknum: self.seqnum_last_received,
             datagram: false, // Might be overwritten later
         };
+        self.counter = (self.counter + 1) % (2u8.pow(6));
 
         if let Some(sent_datagram_packet) = &self.sent_datagram_packet
             && self.cycles_since_datagram_send >= 1
@@ -204,5 +204,326 @@ impl<
         self.stat_tracker
             .sent(header.counter as u32, Instant::now());
         packet
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::protocol::deku_helpers::{PacketPacking, PacketUnpacking};
+    use crate::protocol::proto2025::RadioProtocol2025;
+    use crate::protocol::proto2025::datagrams::{
+        CommandDatagram, CommandDatagramType, EchoDatagram, ResponseDatagram, ResponseDatagramType,
+    };
+    use crate::protocol::proto2025::packet::{
+        DatagramPayload, PACKET_SIZE, PAYLOAD_SIZE, PacketHeader,
+    };
+    use crate::protocol::{PacketRxResult, RadioProtocol};
+    use std::time::Instant;
+
+    // Type aliases to reduce the bloat of the fully qualified call syntax.
+    // The called functions never use all generics at once, so rust can't choose
+    // the right trait implementation without it.
+    type R = [u8; PAYLOAD_SIZE];
+    type DC = CommandDatagram;
+    type DR = ResponseDatagram;
+
+    #[test]
+    fn simple_send() {
+        let mut proto = RadioProtocol2025::new();
+        let test_payload: [u8; PAYLOAD_SIZE] = std::array::from_fn(|i| i as u8);
+
+        let send_buf = RadioProtocol::<R, R, DC, DR>::next_packet(&mut proto, test_payload);
+
+        let header = PacketHeader::unpack(&send_buf[..1]).unwrap();
+
+        assert_eq!(send_buf.len(), PACKET_SIZE);
+        assert_eq!(send_buf[1..], test_payload);
+        assert_eq!(
+            header,
+            PacketHeader {
+                counter: 0,
+                acknum: 1,
+                datagram: false,
+            }
+        );
+    }
+
+    /// Full send- and receive cycle of a large multi-packet datagram
+    #[test]
+    fn datagram_echo() {
+        let mut proto = RadioProtocol2025::new();
+
+        // Build the test packets
+        let test_payload: [u8; PAYLOAD_SIZE] = std::array::from_fn(|i| i as u8);
+        let test_datagram = EchoDatagram {
+            data: std::array::from_fn(|i| i as u8),
+        };
+
+        RadioProtocol::<R, R, DC, DR>::queue_datagram(
+            &mut proto,
+            CommandDatagram::Echo(test_datagram),
+        );
+
+        // ======== Send the echo datagram packet ========
+
+        let mut commands_without_datagram = 0;
+        let mut total_commands = 0;
+        let mut expected_next_byte = 0u8;
+        let mut seqnum_last_recv = 0u8;
+        // Continue sending until there are no more queued chunks
+        while commands_without_datagram < 10 && total_commands < 500 {
+            let send_buf = RadioProtocol::<R, R, DC, DR>::next_packet(&mut proto, test_payload);
+            total_commands += 1;
+
+            let header = PacketHeader::unpack(&send_buf[..1]).unwrap();
+
+            if header.datagram {
+                commands_without_datagram = 0;
+
+                // Validate the command datagram packet
+                assert_eq!(
+                    send_buf.len(),
+                    PACKET_SIZE,
+                    "Sent a command datagram packet with the wrong size"
+                );
+                let datagram_payload =
+                    DatagramPayload::<CommandDatagramType>::unpack(&send_buf[1..]).unwrap();
+                let payload_data = datagram_payload.data;
+                assert_eq!(
+                    datagram_payload.datagram_type,
+                    CommandDatagramType::Echo,
+                    "Sent wrong datagram type"
+                );
+                for byte in payload_data.iter() {
+                    if expected_next_byte < 100 {
+                        assert_eq!(
+                            *byte, expected_next_byte,
+                            "Sent wrong datagram chunk. Could also be a resend of a previous chunk."
+                        );
+                    }
+                    expected_next_byte += 1;
+                }
+
+                // Simulate receiving ack
+                seqnum_last_recv = datagram_payload.seqnum;
+                let mut ack_packet = [0u8; PACKET_SIZE];
+                let ack_header = PacketHeader {
+                    counter: header.counter,
+                    acknum: seqnum_last_recv,
+                    datagram: false,
+                };
+                ack_header.pack_to_slice(&mut ack_packet); // The rest is filled with 0
+
+                assert!(matches!(
+                    RadioProtocol::<R, R, DC, DR>::packet_received(
+                        &mut proto,
+                        &ack_packet,
+                        Instant::now()
+                    ),
+                    PacketRxResult::Regular(_)
+                ));
+            } else {
+                commands_without_datagram += 1;
+            }
+        }
+
+        assert!(total_commands < 500, "Infinitely sending datagram chunks");
+        assert!(
+            expected_next_byte >= 100,
+            "Didn't finish sending the echo packet"
+        );
+
+        // ======== Send the response datagram ========
+
+        let mut received_datagram = None;
+        let mut response_sends = 0;
+        let mut next_response_base_byte = 0;
+        let mut resp_seqnum = 0;
+        while response_sends < 500 && received_datagram.is_none() {
+            // Simulate a command packet to respond to
+            let send_buf = RadioProtocol::<R, R, DC, DR>::next_packet(&mut proto, test_payload);
+            response_sends += 1;
+
+            // Validate the command packet
+            let header = PacketHeader::unpack(&send_buf[..1]).unwrap();
+            assert_eq!(
+                send_buf.len(),
+                PACKET_SIZE,
+                "Sent a regular packet with the wrong size"
+            );
+            assert!(
+                !header.datagram,
+                "Sent datagram chunk when expecting regular packet"
+            );
+            assert_eq!(send_buf[1..], test_payload, "Sent wrong regular packet");
+
+            let mut resp_packet = [0u8; PACKET_SIZE];
+            let resp_header = PacketHeader {
+                counter: header.counter,
+                acknum: seqnum_last_recv, // Keep sending the last acknum
+                datagram: true,
+            };
+            let resp_payload = DatagramPayload::<ResponseDatagramType> {
+                seqnum: resp_seqnum, // Irrelevant for this test
+                datagram_type: ResponseDatagramType::Echo,
+                data: std::array::from_fn(|i| next_response_base_byte + i as u8),
+            };
+            next_response_base_byte += resp_payload.data.len() as u8;
+            resp_header.pack_to_slice(&mut resp_packet[0..1]);
+            resp_payload.pack_to_slice(&mut resp_packet[1..]);
+            resp_seqnum = 1 - resp_seqnum;
+
+            let response: PacketRxResult<[u8; PAYLOAD_SIZE], ResponseDatagram> =
+                RadioProtocol::<R, R, DC, DR>::packet_received(
+                    &mut proto,
+                    &resp_packet,
+                    Instant::now(),
+                );
+            assert!(
+                match response {
+                    PacketRxResult::IncompleteDatagram => {
+                        true
+                    }
+                    PacketRxResult::Datagram(ResponseDatagram::Echo(d)) => {
+                        received_datagram = Some(d);
+                        true
+                    }
+                    _ => false,
+                },
+                "Processed a response datagram chunk incorrectly"
+            )
+        }
+
+        assert!(
+            received_datagram.is_some(),
+            "Didn't receive the full echo response"
+        );
+        assert_eq!(
+            received_datagram.unwrap().data,
+            std::array::from_fn(|i| i as u8),
+            "Received wrong echo data"
+        );
+    }
+
+    /// 0-sized datagrams like ReadRobotId should be handled correctly by sending a dummy chunk.
+    #[test]
+    fn empty_datagram() {
+        let mut proto = RadioProtocol2025::new();
+        let test_payload: [u8; PAYLOAD_SIZE] = std::array::from_fn(|i| i as u8);
+        let test_datagram = CommandDatagram::ReadRobotId;
+
+        RadioProtocol::<R, R, DC, DR>::queue_datagram(&mut proto, test_datagram);
+
+        let send_buf = RadioProtocol::<R, R, DC, DR>::next_packet(&mut proto, test_payload);
+        let header = PacketHeader::unpack(&send_buf[..1]).unwrap();
+
+        assert!(header.datagram, "Expected a datagram packet");
+
+        let datagram_payload =
+            DatagramPayload::<CommandDatagramType>::unpack(&send_buf[1..]).unwrap();
+        assert_eq!(
+            datagram_payload.datagram_type,
+            CommandDatagramType::ReadRobotId
+        );
+        assert_eq!(
+            datagram_payload.data,
+            [0u8; crate::protocol::proto2025::packet::DATAGRAM_CHUNK_SIZE],
+            "Expected an empty chunk. Still works, but there is probably a bug somewhere."
+        );
+    }
+
+    /// Datagrams must be retransmitted if no ack is received in time
+    #[test]
+    fn datagram_resend() {
+        let mut proto = RadioProtocol2025::new();
+        let test_payload: [u8; PAYLOAD_SIZE] = std::array::from_fn(|i| i as u8);
+
+        RadioProtocol::<R, R, DC, DR>::queue_datagram(&mut proto, CommandDatagram::ReadRobotId);
+
+        // Expect initial send within 10 cycles
+        let mut sent_initial = false;
+        for _ in 0..10 {
+            let send_buf = RadioProtocol::<R, R, DC, DR>::next_packet(&mut proto, test_payload);
+            let header = PacketHeader::unpack(&send_buf[..1]).unwrap();
+            if header.datagram {
+                let init_payload =
+                    DatagramPayload::<CommandDatagramType>::unpack(&send_buf[1..]).unwrap();
+                assert_eq!(init_payload.datagram_type, CommandDatagramType::ReadRobotId);
+                sent_initial = true;
+                break;
+            }
+        }
+        assert!(sent_initial, "Didn't send the initial datagram");
+
+        // Expect resend within 10 cycles
+        let mut got_resend = false;
+        for _ in 0..10 {
+            let send_buf = RadioProtocol::<R, R, DC, DR>::next_packet(&mut proto, test_payload);
+            let header = PacketHeader::unpack(&send_buf[..1]).unwrap();
+            if header.datagram {
+                let payload =
+                    DatagramPayload::<CommandDatagramType>::unpack(&send_buf[1..]).unwrap();
+                assert_eq!(
+                    payload.datagram_type,
+                    CommandDatagramType::ReadRobotId,
+                    "Resend should have the same datagram type"
+                );
+                got_resend = true;
+            }
+        }
+
+        assert!(got_resend, "Didn't resend a \"lost\" datagram");
+    }
+
+    /// Multiple datagrams should be queueable at once. Depends on empty_datagrams
+    #[test]
+    fn datagram_queue() {
+        let mut proto = RadioProtocol2025::new();
+        let test_payload: [u8; PAYLOAD_SIZE] = std::array::from_fn(|i| i as u8);
+        let test_datagram1 = CommandDatagram::ReadRobotId;
+        let test_datagram2 = CommandDatagram::ReadBoardIds;
+
+        RadioProtocol::<R, R, DC, DR>::queue_datagram(&mut proto, test_datagram1);
+        RadioProtocol::<R, R, DC, DR>::queue_datagram(&mut proto, test_datagram2);
+
+        assert_eq!(proto.datagram_queue.len(), 2);
+
+        let mut seen_datagram_types = Vec::new();
+        for _ in 0..1000 {
+            let send_buf = RadioProtocol::<R, R, DC, DR>::next_packet(&mut proto, test_payload);
+            let header = PacketHeader::unpack(&send_buf[..1]).unwrap();
+
+            if header.datagram {
+                let datagram_payload =
+                    DatagramPayload::<CommandDatagramType>::unpack(&send_buf[1..]).unwrap();
+                if !seen_datagram_types.contains(&datagram_payload.datagram_type) {
+                    seen_datagram_types.push(datagram_payload.datagram_type);
+                }
+
+                let mut ack_packet = [0u8; PACKET_SIZE];
+                let ack_header = PacketHeader {
+                    counter: header.counter,
+                    acknum: datagram_payload.seqnum,
+                    datagram: false,
+                };
+                ack_header.pack_to_slice(&mut ack_packet);
+
+                RadioProtocol::<R, R, DC, DR>::packet_received(
+                    &mut proto,
+                    &ack_packet,
+                    Instant::now(),
+                );
+            }
+
+            if seen_datagram_types.len() == 2 {
+                break;
+            }
+        }
+
+        assert!(
+            seen_datagram_types.contains(&CommandDatagramType::ReadRobotId)
+                && seen_datagram_types.contains(&CommandDatagramType::ReadBoardIds),
+            "Didn't receive both datagram types"
+        );
     }
 }
